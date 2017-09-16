9f6e192d1de850034e18a5d278ebf10c3eab79a2
Jason <kline.379@osu.edu>
2017-09-16 01:11:21 -0400

    more test stuff.

--- src/test/java/hello/GreetingTest.java
+++ src/test/java/hello/GreetingTest.java
@@ -9,21 +9,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(GreetingController.class)
-//tag::test[]
-public class WebLayerTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GreetingControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello World")));
+        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Daytona Beach")));
     }
 }
