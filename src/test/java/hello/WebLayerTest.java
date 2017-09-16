7bcd612c47ed9ff65abb1563bfd05629236c0a28
Jason <kline.379@osu.edu>
2017-09-16 00:44:39 -0400

    tests.

---
+++ src/test/java/hello/WebLayerTest.java
@@ -0,0 +1,13 @@
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class WebLayerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello World")));
+    }
+}
