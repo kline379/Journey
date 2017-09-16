e4e1c8f5a49720a01e02b21a8a7ebb311d5bd2e6
Jason <kline.379@osu.edu>
2017-09-16 00:21:12 -0400

    Testing test framework.

---
+++ src/test/java/hello/GreetingControllerTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import org.junit.Assert;
+
+public class GreetingControllerTest{
+    @Test
+    public void notYetImplemented(){
+        Assert.fail("Not Yet Implemented");
+    }
+}
