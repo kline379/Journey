422ac89d5c691fda2a3ccf4517f7436f3daa36f5
josh.sandvick <josh.sandvick@gmail.com>
2017-09-25 19:06:18 -0400

    Java now interfaces with Solr and Watson

---
+++ src/main/java/backend/QueryRetriever.java
@@ -0,0 +1,57 @@
+package backend;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.http.client.utils.HttpClientUtils;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocumentList;
+
+import com.ibm.watson.developer_cloud.retrieve_and_rank.v1.RetrieveAndRank;
+import com.ibm.watson.developer_cloud.retrieve_and_rank.v1.model.SolrCluster;
+import com.ibm.watson.developer_cloud.retrieve_and_rank.v1.model.SolrClusters;
+import com.ibm.watson.developer_cloud.retrieve_and_rank.v1.model.SolrCluster.Status;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class QueryRetriever 
+{    
+    public SolrDocumentList RetrieveQueries() throws InterruptedException, SolrServerException, IOException{
+    	
+    	final String USERNAME = "638bd9ff-2179-469b-93ad-7ca894776fdd";
+    	final String PASSWORD = "8slDdIXgCKjd";
+    	
+    	RetrieveAndRank service = new RetrieveAndRank();
+    	service.setUsernameAndPassword(USERNAME, PASSWORD);
+    	
+    	// Grab the Solr cluster
+    	List<SolrCluster> clusters = service.getSolrClusters().execute().getSolrClusters();
+    	SolrCluster cluster = clusters.get(0);
+    	
+    	while(cluster.getStatus() == Status.NOT_AVAILABLE) {
+    		Thread.sleep(10000); // sleep 10 seconds
+    		cluster = service.getSolrCluster(cluster.getId()).execute();
+    	}	
+    	
+    	System.out.println("Cluster is available!");
+    	
+    	// Make a query
+    	String endPoint = "https://gateway.watsonplatform.net/retrieve-and-rank/api";
+    	HttpSolrClient solrClient = new HttpSolrClient(service.getSolrUrl(cluster.getId()),
+    	        HttpSolrClientUtils.createHttpClient(endPoint, USERNAME, PASSWORD));
+    	SolrQuery query = new SolrQuery("beach and race track");
+    	QueryResponse response = solrClient.query("Wiki_Travel", query);
+    	
+    	return response.getResults(); //.get(0).getFieldValue("title"));
+    	
+    	//System.out.println(response);
+    }
+}
+
--- src/main/java/frontend/ResultCardsController.java
+++ src/main/java/frontend/ResultCardsController.java
@@ -7,75 +7,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 import java.util.ArrayList;
 import java.io.*;
+import backend.QueryRetriever;
+import org.apache.solr.common.SolrDocumentList;
 
 @Controller
 public class ResultCardsController {
 
   @RequestMapping("/results")
-  public String populateCards(
-      @RequestParam(value = "searchQuery", required = true, defaultValue = "World") String query, Model model) {
+  public String populateCards (
+      @RequestParam(value = "searchQuery", required = true, defaultValue = "World") String query, Model model) 
+    		  throws Exception {
 
     List<Card> results = process(query);
     model.addAttribute("results", results);
     return "cards";
   }
 
-  private List<Card> process(String query) {
-    try {
-
-      ProcessBuilder ps = new ProcessBuilder(
-        "python",
-        "src/main/java/frontend/query.py"
-      );
-
-      ps.redirectErrorStream(true);
-      Process p = ps.start();
-
-      List<Card> cards = new ArrayList<Card>();
-
-      BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
-
-      writer.write(query + "\n");
-      writer.flush();
-
-      try (BufferedReader oR = new BufferedReader(new InputStreamReader(p.getInputStream()));) {
-        String s = oR.readLine();
-
-        while (s != null) {
-          String title = s;
-          StringBuilder body = new StringBuilder();
-          s = oR.readLine();
-
-          while (!s.equals("###")) {
-            body.append(s);
-            s = oR.readLine();
-          }
-
-          cards.add(new Card(title, body.toString()));
-          s = oR.readLine();
-        }
-      }
-      p.waitFor();
-      return cards;
-    } catch (Throwable e) {
-      List<Card> cards = new ArrayList<Card>();
-      cards.add(new Card("Error", e.toString()));
-      return cards;
-    }
-
-    /*
-    List<Card> cards = new ArrayList<Card>();
-    Card zero = new Card(query, "This should be here too");
-    Card one = new Card("Myrtle Beach, SC", "One beach");
-    Card two = new Card("Daytona Beach, FL", "Two Beach");
-    Card three = new Card("Miami, FL", "Red Beach");
-    Card four = new Card("Malibu, CA", "Blue Beach");
-    cards.add(zero);
-    cards.add(one);
-    cards.add(two);
-    cards.add(three);
-    cards.add(four);
-    return cards;
-    */
+  private List<Card> process(String query) throws Exception {
+	  List<Card> cardList = new ArrayList<Card>();
+	  
+	  QueryRetriever retriever = new QueryRetriever();
+	  SolrDocumentList documents = retriever.RetrieveQueries();
+	  for(int i = 0; i < documents.size(); i++) {
+		  String title = documents.get(i).getFieldValue("title").toString();
+		  cardList.add(new Card(title, ""));
+	  }
+	  
+	  return cardList;
   }
 }
