754e8d7bfd05c793d80a0968de3163b7faef1e90
unknown <roadeo7@gmail.com>
2017-09-26 16:55:27 -0400

    rename test file packages

--- src/test/java/frontend/GreetingControllerTest.java
+++ src/test/java/frontend/GreetingControllerTest.java
@@ -1,4 +1,4 @@
-package hello;
+package frontend;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -17,13 +17,14 @@ import org.springframework.test.web.servlet.MockMvc;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ApplicationTest {
+public class GreetingControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
+        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Daytona Beach")));
     }
 }
