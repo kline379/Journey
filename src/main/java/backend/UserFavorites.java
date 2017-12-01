3232e8f325a1ea389385aa5dd93aa5eb2e5bcf3f
Dan Shawlson <danshawlson@gmail.com>
2017-11-30 19:10:47 -0500

    Implement favorites fetcher

--- src/main/java/backend/UserFavorites.java
+++ src/main/java/backend/UserFavorites.java
@@ -29,45 +29,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 public class UserFavorites {
 
-  @Autowired
-  DynamoDB client;
-
   public static List<Integer> getFavorites(String user) {
 
-    return new ArrayList<Integer>();
+    List<Integer> ids = new ArrayList<Integer>();
 
-  //   try {
-  //     // See if the banner is already in the database
-  //     GetItemSpec spec = new GetItemSpec().withPrimaryKey("email", title);
-  //     Item outcome = this.bannerTable.getItem(spec);
-  //     if (outcome == null) {
-  //       // Throw an Exception if it's not
-  //       throw new Exception(title+ ": No such entry");
-  //     }
-  //     return outcome.get("url").toString();
-  //   }
-  //   catch (Exception e) {
-  //     // The banner was not in the database
-  //     System.err.println("Unable to read item");
-  //     System.err.println(e.getMessage());
-  //     // Scrape it from WikiTravel
-  //     String imageURL = scrapeImageURL(title);
-  //     try {
-  //       // Write the scraped banner url to the database
-  //       Item item = new Item()
-  //         .withPrimaryKey("title", title)
-  //         .withString("url", imageURL);
-  //       this.bannerTable.putItem(item);
-  //     }
-  //     catch (Exception nestedE) {
-  //       System.err.println("Error writing (" + title + "," + imageURL + ") to table");
-  //       System.err.println(nestedE.getMessage());
-  //     }
-  //     return imageURL;
-  //   }
-  // }
+    AWSCredentials awsCreds = new BasicAWSCredentials(
+      "AKIAILFG7TKL3Y256ATA", "HpY/jI19qjnqT2EDQiJlbw9Y5ij7ETq3pUmWl387");
+    AmazonDynamoDB client = AmazonDynamoDBClientBuilder
+      .standard()
+      .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+      .withRegion(Regions.US_EAST_1)
+      .build();
+    DynamoDB db = new DynamoDB(client);
+
+    try {
+      Table userTable = db.getTable("users");
+      GetItemSpec spec = new GetItemSpec().withPrimaryKey("email", user);
+      Item outcome = userTable.getItem(spec);
+      if (outcome == null) {
+        throw new Exception(user + ": No such entry");
+      }
+
+      for (BigDecimal number : outcome.getNumberSet("favorites")) {
+        ids.add(number.intValue());
+      }
+    } catch (Exception e) {
+      System.err.println("Unable to read favorites of user " + user);
+      System.err.println(e.getMessage());
+    } finally {
+      return ids;
+    }
+  }
 }
